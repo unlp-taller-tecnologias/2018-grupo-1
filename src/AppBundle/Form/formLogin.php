32788ce33e6adfd5e10264d1e36bfe0d3a016da1
pablo <pablo.95-09@hotmail.com>
2018-10-27 18:52:46 -0300

    2018-10-27 commit inicial

---
+++ src/AppBundle/Form/formLogin.php
@@ -0,0 +1,30 @@
+<?php 
+
+namespace AppBundle\Form;
+
+use AppBundle\Entity\Post;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+
+class formLogin extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('login')
+            ->add('dni', TextareaType::class)
+            ->add('password', PasswordType::class)
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => Post::class,
+        ));
+    }
+}
+
+?>
