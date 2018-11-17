9c419008f1a6bc5d0898af82533b6083b0ebc977
Matias <matiaspompilio97@gmail.com>
2018-11-17 12:37:51 -0300

    count perimetral

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -5,6 +5,7 @@ namespace AppBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use AppBundle\Entity\Perimetral;
 
 class DefaultController extends Controller
 {
@@ -13,9 +14,15 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
+      $repository = $this->getDoctrine()->getRepository(Perimetral::class);
+      $medidasVencidas= count($repository->getVencidas());
+      $medidasVencer=count($repository->getVencer(5));
+     return $this->render('default/index.html.twig', array('medidasVencidas'=> $medidasVencidas, 'medidasVencer' => $medidasVencer ));
         // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
-        ]);
+        // return $this->render('default/index.html.twig', [
+        //     'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+        // ]);
     }
+
+
 }
