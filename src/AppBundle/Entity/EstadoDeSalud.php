10b65cd235cf99ea1a2a6b359662806e310daf07
Fernando <sn2005ap@gmail.com>
2018-12-01 00:19:49 -0300

    fixes

--- src/AppBundle/Entity/EstadoDeSalud.php
+++ src/AppBundle/Entity/EstadoDeSalud.php
@@ -5,7 +5,6 @@ namespace AppBundle\Entity;
 use Doctrine\ORM\Mapping as ORM;
 use Doctrine\Common\Collections\ArrayCollection;
 use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
-use Doctrine\Common\Collections\ArrayCollection;
 
 /**
  * EstadoDeSalud
